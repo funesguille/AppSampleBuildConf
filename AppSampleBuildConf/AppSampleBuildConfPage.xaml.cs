4093c71de8a35d069753211e61dc543f5850d57d
Roberto Cervantes <rcervantes@outlook.com>
2018-02-20 16:39:51 -0600

    Initial commit

---
+++ AppSampleBuildConf/AppSampleBuildConfPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace AppSampleBuildConf
+{
+    public partial class AppSampleBuildConfPage : ContentPage
+    {
+        public AppSampleBuildConfPage()
+        {
+            InitializeComponent();
+#if DEBUG
+            message.Text = "Ejecución en Debug";
+#else
+            message.Text = "Ejecución en Release";
+#endif
+        }
+    }
+}
